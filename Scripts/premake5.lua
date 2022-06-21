f9451a5d7922c49131ed17bdac9133dcbf13f2e2
Daversini <bersini.davide@gmail.com>
2022-06-21 02:27:25 +0200

    Moved all scripts into a separate folder

--- Scripts/premake5.lua
+++ Scripts/premake5.lua
@@ -1,6 +1,7 @@
 include "premake5-Dependencies.lua"
 
 workspace "BersiniDavide_EndYearExamination"
+    location "../"
     architecture "x86_64"
     startproject "Sandbox"
 
@@ -12,5 +13,5 @@ workspace "BersiniDavide_EndYearExamination"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
-include "Framework"
-include "Sandbox"
+include "../Framework"
+include "../Sandbox"
